144f73d5712cece44c09aa1ad844c1ce2b07840d
Sebastián Ulloa <seba@sebastiann.me>
2022-01-06 15:15:33 -0300

    Initial app setup, Zeitwerk and dockerized services running

---
+++ config/initializers/background.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative 'main'
+
+# If you have background/actions to run with cron, schedule here
+# Ex: Sidekiq::Cron::Job.create(name: 'ExampleJob',
+#                               cron: '*/5 * * * *',
+#                               class: 'ExampleJob')
